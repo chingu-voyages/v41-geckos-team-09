aea7c8f25ec37a278b7b50d82966060989748fe1
Sara Baqla <missatrox44@gmail.com>
2022-10-15 13:24:10 -0500

    rough draft header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/header/header'
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
     </div>
   )
 }
