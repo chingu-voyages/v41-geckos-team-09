f70a39b2302c04a9c9bf63f88e579e56a150bd00
“isabelklint” <isabel.klint@gmail.com>
2022-10-20 16:56:49 -0500

    added Thomas' linked in link, removed andy

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,10 +6,15 @@ const Footer = () => {
 <footer className ="footerContainer">
             <h4>Travel2it is coded by 
                 <a className="custom-link" target="_blank" href='https://www.linkedin.com/in/sara-baqla/' title="Sara, a linkedin.com link"> Sara
-                </a>, Ian, Thomas, Andy, and
+                </a>, Ian, 
+                
+                Thomas, 
+
+                <a className="custom-link" target="_blank" href='https://www.linkedin.com/in/thomas-ruppeiner-201880170/' title="Thomas, a linkedin.com link"> Thomas
+                </a>, and
                 <a className="custom-link" target="_blank" href='https://www.linkedin.com/in/isabel-klint/' title="Isabel, a linkedin.com link"> Isabel
                 </a>. See our project on
-                <a className="custom-link" target="_blank" href='https://github.com/chingu-voyages/v41-geckos-team-09' title="Github link"> Github.
+                <a className="custom-link" target="_blank" href='https://github.com/chingu-voyages/v41-geckos-team-09' title="the Travel 2 It Github link"> Github.
                 </a>
             </h4>
 </footer>
