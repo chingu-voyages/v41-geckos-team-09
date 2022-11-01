910d7d1aa7e222b2c79dde17649e0129cf17e1ff
“isabelklint” <isabel.klint@gmail.com>
2022-11-01 16:04:11 -0600

    add margin to welcome page

--- src/components/welcome/welcome.jsx
+++ src/components/welcome/welcome.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Box, Heading, Text } from '@chakra-ui/react'
+import { Box, Heading, Text, Flex } from '@chakra-ui/react'
 
 export default function Welcome() {
   return (
-    <Box padding={'8px'} alignContent={'center'}>
+    <Box  w={'80%'}
+    m={'20px'} 
+    p={'20px'}
+    bg={'inherit'} 
+    >
       <Heading textColor={'#1779EA'}> Welcome to Travel To It</Heading>
       <Text>Landing Page instructions will go here. Users will click on logo to return to this page.</Text>
     </Box>
