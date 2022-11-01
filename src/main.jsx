a4fe8bf0a0ceab2d1f98e64bff6d67b5b720507e
“isabelklint” <isabel.klint@gmail.com>
2022-10-31 18:35:14 -0600

    moved chakra to main

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <App />
-
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
 )
