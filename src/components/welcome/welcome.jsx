61a32baf5d0edd1ddc96f937dc4bc89c72ae2cd6
“isabelklint” <isabel.klint@gmail.com>
2022-11-01 09:56:07 -0600

    make welcome h1 into chakra heading #1779EA

--- src/components/welcome/welcome.jsx
+++ src/components/welcome/welcome.jsx
@@ -4,7 +4,7 @@ import { Heading } from '@chakra-ui/react'
 export default function Welcome() {
   return (
     <div>
-      <Heading>Welcome to Travel To It</Heading>
+      <Heading textColor={'#1779EA'} > Welcome to Travel To It</Heading>
       <p>Landing Page instructions will go here. Users will click on logo to return to this page.</p>
     </div>
   )
