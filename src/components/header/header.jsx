dbdf00285b23bdb74a1fc174864b0d5f5d5b3c19
“isabelklint” <isabel.klint@gmail.com>
2022-11-03 16:52:17 -0600

    add toggle button to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './header.css';
 import logo from '../../images/logo-1.png';
-import { Box, Flex, Spacer, Link, Select} from '@chakra-ui/react'
-// import ToggleMode from '../toggle.jsx'
-// import theme from '../theme'
+import { Box, Flex, Spacer, Link, Select } from '@chakra-ui/react'
+import ToggleMode from '../toggle.jsx'
+import theme from '../theme'
 
 import {
   EmailShareButton,
@@ -46,14 +46,16 @@ const whatsAppURL = 'https://www.whatsapp.com/';
         <WhatsappShareButton url={whatsAppURL}><WhatsappIcon size={45} borderRadius={15} /></WhatsappShareButton>
       </Box>
       <Spacer />
-      <Box m={'17px'} size={45} borderRadius={15}>
+      <Box >
         {/* <Select className='menu'> */}
-        <Select placeholder='My Trip'>
+        <Select textColor={'#FAF9F9'} placeholder='My Trip' mt={'17px'} h={10} borderRadius={10} border={'none'} bgColor={'#1679ea'}>
           <option value='#'>Timeline View</option>
           <option value='#'>Stack View</option>
           <option value='#'>Share</option>
         </Select>
       </Box>
+      <Spacer/>
+      <Box><ToggleMode mt="md">color mode</ToggleMode></Box>
     </Flex>
   )
 }
