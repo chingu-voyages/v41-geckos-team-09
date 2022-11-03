b15735e5b3ad1f7409346c9f0e25f110007043c0
“isabelklint” <isabel.klint@gmail.com>
2022-11-03 09:17:04 -0600

    add fonts (attempt)

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,7 +1,12 @@
 // theme.js
 
 // 1. import `extendTheme` function
-import { extendTheme } from '@chakra-ui/react'
+import { 
+  extendTheme,
+  theme as base,
+  withDefaultColorScheme,
+  withDefaultVariant,
+} from '@chakra-ui/react'
 
 // 2. Add your color mode config
 const config = {
@@ -11,17 +16,23 @@ const config = {
 
 const button = {
   size: 'xs',
-  bg: '#BBBBBB'
+  bgColor: '#AAAAAA'
 }
 
 const stackHeading = {
   size: 'lg',
   padding: '80px',
   margin: '20px',
-  color: 'inherit'
+  bgColor: '#AAAAAA'
 }
 
+
+
 // 3. extend the theme
-const theme = extendTheme({ config, button, stackHeading })
+const theme = extendTheme({ 
+  config, 
+  button, 
+  stackHeading, 
+})
 
 export default theme
