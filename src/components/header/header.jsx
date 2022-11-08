003cca549ae29c17bde51e48c391c7e7af301fa3
“isabelklint” <isabel.klint@gmail.com>
2022-11-08 17:11:40 -0600

    added chakra elements to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './header.css';
 import logo from '../../images/logo-1.png';
+import { Box, Flex, Spacer, Link, Select } from '@chakra-ui/react'
 import ToggleMode from '../toggle'
-import { Box } from '@chakra-ui/react'
-
 
 import {
   EmailShareButton,
@@ -21,40 +20,40 @@ const whatsAppURL = 'https://www.whatsapp.com/';
 // const emailURL = ''
 
   return (
-    <header className='dFlex'>
-      <a id='logoLink' href='#Welcome' onClick={() => handleViewChange('Welcome')}><img className='header-logo' src={logo} alt='travel logo' /></a>
-      <div className='nav'>
-        <ul className='links'>
-          <li><a className='custom-headerLink' href='#Timeline' onClick={() => handleViewChange('Timeline')}>Timeline View</a></li>
-          <li><a className='custom-headerLink' href='#Board' onClick={() => handleViewChange('Board')}>Stack View</a></li>
-          {/* <li><a className='custom-headerLink' href='#'>Share</a></li>  */}
-          {/* dynamically render Trip Name when user names file */}
-          <li><a className='hidden' href='#'>Trip Name</a></li>
-          <li className='shareIcon'>
-            {/* Email share button currently NOT functional */}
-            <EmailShareButton><EmailIcon size={45} borderRadius={15} /></EmailShareButton>
-          </li>
-          <li className='shareIcon'>
-            <FacebookShareButton url={facebookURL}><FacebookIcon size={45} borderRadius={15} /></FacebookShareButton>
-          </li>
-          <li className='shareIcon'>
-            <WhatsappShareButton url={whatsAppURL}><WhatsappIcon size={45} borderRadius={15} /></WhatsappShareButton>
-          </li>
-        </ul>
-
-        <div className='custom-select'>
-          <select className='menu'>
-            <option value='' selected='selected'>My Trip</option>
-
-            <option value='#'>Timeline View</option>
-            <option value='#'>Stack View</option>
-            <option value='#'>Share</option>
-          </select>
-        </div>
-        <Box><ToggleMode mt="md">color mode</ToggleMode></Box>
-
-      </div>
-
-    </header>
+    <Flex shadow='md
+    '>
+      <Box><Link id='logoLink' href='#Welcome' onClick={() => handleViewChange('Welcome')}><img className='header-logo' src={logo} alt='travel logo' /></Link></Box>
+      <Spacer />
+      <Box mt={'17px'} p={'1em'}><Link _hover={{ fontSize:'xl', color: '#aa5d89' }}  href='#Welcome' onClick={() => handleViewChange('Welcome')}>Welcome View</Link></Box>
+      <Spacer />
+      <Box mt={'17px'} p={'1em'} ><Link _hover={{ fontSize:'xl', color: '#aa5d89' }} href='#Board' onClick={() => handleViewChange('Board')}>Board View</Link></Box>
+      <Spacer />
+      <Box mt={'17px'} p={'1em'} ><Link _hover={{ fontSize:'xl', color: '#aa5d89' }} href='#'>Share</Link></Box>
+      <Spacer />
+      <Box className='shareIcon' mt={'17px'}>
+        {/* Email share button currently NOT functional */}
+        <EmailShareButton><EmailIcon size={45} borderRadius={15} />
+        </EmailShareButton>
+      </Box>
+      <Spacer />
+      <Box className='shareIcon' m={'17px'}>
+        <FacebookShareButton url={facebookURL}><FacebookIcon size={45} borderRadius={15} /></FacebookShareButton>
+      </Box>
+      <Spacer />
+      <Box className='shareIcon' mt={'17px'}>
+        <WhatsappShareButton url={whatsAppURL}><WhatsappIcon size={45} borderRadius={15} /></WhatsappShareButton>
+      </Box>
+      <Spacer />
+      <Box >
+        {/* <Select className='menu'> */}
+        <Select textColor={'#FAF9F9'} placeholder='My Trip' mt={'17px'} h={10} borderRadius={10} border={'none'} bgColor={'#1679ea'}>
+          <option value='#'>Timeline View</option>
+          <option value='#'>Stack View</option>
+          <option value='#'>Share</option>
+        </Select>
+      </Box>
+      <Spacer/>
+      <Box><ToggleMode mt="md">color mode</ToggleMode></Box>
+    </Flex>
   )
 }
