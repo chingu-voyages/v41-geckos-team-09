6ab719a88ac7c237ee06a4042b9d4f66c04a3a08
“isabelklint” <isabel.klint@gmail.com>
2022-11-08 16:57:49 -0600

    add chakra to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+        <ChakraProvider >
+            <ColorModeScript />
+            <App />
+        </ChakraProvider>
 )
