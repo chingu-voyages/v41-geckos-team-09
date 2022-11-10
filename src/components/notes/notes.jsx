2e48428606c3de50034cbb650e9c27637a7118ee
“isabelklint” <isabel.klint@gmail.com>
2022-11-09 22:09:09 -0600

    enlarge notes area

--- src/components/notes/notes.jsx
+++ src/components/notes/notes.jsx
@@ -3,12 +3,30 @@ import { Box, Textarea, Flex, Heading } from '@chakra-ui/react'
 
 
 export default function Notes() {
+  let [value, setValue] = React.useState('')
+
+  let handleInputChange = (e) => {
+    let inputValue = e.target.value
+    // setValue(inputValue)
+  }
   return (
+    <>
     <Flex >
       <Box>
       <Heading>Notes</Heading>
-      <Textarea borderColor={'#DB095B'}>Write some notes</Textarea>
+      <Textarea transition="height none" 
+        borderColor={'#DB095B'} 
+        // value={value}
+        onChange={handleInputChange}
+        minH="unset"
+        overflow="hidden"
+        value={value}
+        resize="none"
+        w={"900px"}
+        h={"200px"}
+        placeholder='Write some notes'></Textarea>
       </Box>
     </Flex>
+    </>
   )
 }
