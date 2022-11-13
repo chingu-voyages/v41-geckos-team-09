33330a0461ceb32cd48e2db4c96997b92035c371
Ian Korpi <iankorpi@protonmail.com>
2022-11-13 14:07:19 -0600

    Updated readme and welcome info.

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# Travel Card App (Name TBA)
-
-A shareable vacation planning board serving users with visual impairments.
+# Travel 2 It
+Travel 2 It is a travel planning app based on using cards to plan your travel agenda. It is built using React, Chakra, React Router, and LocalForage, as well as a number of other technologies. Check out `package.json` for a full list of dependencies. It was built as a team project for Chingu Voyage 41.
 
 ## How to run
 ### Locally
@@ -9,3 +8,6 @@ A shareable vacation planning board serving users with visual impairments.
 3. Run `yarn`.
 4. Run `yarn dev`.
 5. Navigate to `http://127.0.0.1:5173/` in your web browser.
+
+### Live Demo
+A live demo is available here: [Live Demo](https://cosmic-gnome-4fc624.netlify.app/).
--- src/components/welcome/welcome.jsx
+++ src/components/welcome/welcome.jsx
@@ -11,7 +11,7 @@ export default function Welcome() {
         <img className='bus' alt='initial instructions' src={bus} />
         <h1 className='welcome-heading'>Plan your next trip with ease using Travel 2 It</h1>
 
-        <p className='welcome-blurb'>Your one stop shop to keep everything organized. Easily share your plans with family and friends.</p>
+        <p className='welcome-blurb'>Your one stop shop to keep everything organized. </p>
       </div>
       <br />
       <br />
@@ -19,9 +19,7 @@ export default function Welcome() {
         <h2 className='instructions-heading'>How does Travel 2 It work?</h2>
         <p className='instructions-text'>Stay organized while planning your trip by creating editable stacks and cards. Each stack can hold as many cards as necessary. Edit card information with places to visit or other potential plans. Unsure of the order? Simply rearrange cards OR stacks by dragging and dropping them into the desired location.</p>
         <br/>
-        <p className='instructions-text'>Dark & Light Mode available! Share with family and friends through Email, Facebook or Whatsapp!</p>
-        <br/>
-        <p className='instructions-text'>Vivamus egestas magna turpis, a tincidunt nisi maximus quis. Aenean sed suscipit erat, in fermentum ex. Pellentesque egestas pellentesque augue in porttitor. Nullam nibh dui, molestie sed nisl vel, commodo ullamcorper arcu. Aenean at tincidunt ante. Morbi blandit turpis vel elementum varius. Sed nec accumsan enim. Duis semper vehicula tincidunt. Phasellus tristique nisi vel condimentum tincidunt.</p>
+        <p className='instructions-text'>Dark & Light Mode available! Sharing with family and friends through Email, Facebook or Whatsapp coming soon!</p>
         <br/>
         <img className='instructions-img' alt='initial instructions' src={howTo} />
         <br/>
